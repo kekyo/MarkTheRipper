b3434d580e10f1fd9869cd51abed7ff2beabfa2e
Kouji Matsui <k@kekyo.net>
2022-10-25 20:46:56 +0900

    Updated readme.

--- MarkTheRipper.Engine/Functions/Iterator.cs
+++ MarkTheRipper.Engine/Functions/Iterator.cs
@@ -20,63 +20,6 @@ namespace MarkTheRipper.Functions;
 
 internal static class Iterator
 {
-    private static async ValueTask<IExpression> GetNextToEntryAsync(
-        bool forward,
-        IExpression[] parameters,
-        IMetadataContext metadata,
-        IReducer reducer,
-        CancellationToken ct)
-    {
-        async ValueTask<IExpression> GetNextToEntryAsync(MarkdownEntry targetEntry)
-        {
-            if (await targetEntry.GetPropertyValueAsync("markdownPath", metadata, reducer, ct) is PathEntry markdownPath &&
-                await targetEntry.GetPropertyValueAsync("date", metadata, reducer, ct) is PartialDateEntry date &&
-                await targetEntry.GetPropertyValueAsync("category", metadata, reducer, ct) is PartialCategoryEntry category &&
-                await category.GetPropertyValueAsync("entries", metadata, reducer, ct) is MarkdownEntry[] entries)
-            {
-                var currentIndex = Array.FindIndex(
-                    entries,
-                    entry => entry.MarkdownPath.Equals(markdownPath));
-                if (currentIndex >= 0)
-                {
-                    static IEnumerable IterateEntries(int offset, MarkdownEntry[] entries, bool forward)
-                    {
-                        if (forward)
-                        {
-                            for (var index = offset + 1; index < entries.Length; index++)
-                            {
-                                yield return entries[index];
-                            }
-                        }
-                        else
-                        {
-                            for (var index = offset - 1; index >= 0; index--)
-                            {
-                                yield return entries[index];
-                            }
-                        }
-                    }
-                    return new ValueExpression(IterateEntries(currentIndex, entries, forward));
-                }
-            }
-
-            return new ValueExpression(null);
-        }
-
-        if (parameters.FirstOrDefault() is { } p0 &&
-            await reducer.ReduceExpressionAsync(p0, metadata, ct) is MarkdownEntry targetEntry1)
-        {
-            return await GetNextToEntryAsync(targetEntry1);
-        }
-        else if (metadata.Lookup("self") is { } self &&
-            await reducer.ReduceExpressionAsync(self, metadata, ct) is MarkdownEntry targetEntry2)
-        {
-            return await GetNextToEntryAsync(targetEntry2);
-        }
-
-        return new ValueExpression(null);
-    }
-
     public static async ValueTask<IExpression> TakeAsync(
         IExpression[] parameters,
         IMetadataContext metadata,
